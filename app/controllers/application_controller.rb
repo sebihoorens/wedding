e85a138e8cb045ba0698dcded2fa7ff38b57974b
Sebi <sebihoorens@gmail.com>
2025-01-17 17:49:22 +0100

    multilinguality!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  around_action :switch_locale
+
   protect_from_forgery with: :exception
 
   if ENV['BASIC_AUTH_NAME'] && ENV['BASIC_AUTH_PASSWORD']
     http_basic_authenticate_with \
       name: ENV['BASIC_AUTH_NAME'],
       password: ENV['BASIC_AUTH_PASSWORD']
+  end
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  scope '/:locale' do
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
 
-  get 'welcome/index'
+    get 'welcome/index'
 
-  resources :guests do
-    resources :plus_ones
-    member do
-      get :confirm
-      patch :complete
+    resources :guests do
+      resources :plus_ones
+      member do
+        get :confirm
+        patch :complete
+      end
     end
+
+    root 'welcome#index'
   end
-
-  root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
