6cf9f8d3254ac6e88a6224a215598dcff1ab53e8
Sebi <sebihoorens@gmail.com>
2025-03-16 20:22:30 +0100

    whenwhere 3 icons + new leader img

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
-    @google_maps_url = 'https://maps.app.goo.gl/C5CQ6MW2hFktH5wA7'
+    @google_maps_guapulo_url = 'https://maps.app.goo.gl/DTiD1FDRUUtRwWHBA'
+    @google_maps_alcazar_url = 'https://maps.app.goo.gl/C5CQ6MW2hFktH5wA7'
   end
 end
