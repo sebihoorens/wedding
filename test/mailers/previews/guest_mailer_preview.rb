e15ad0e1457bcb51d016b8af3e3b219a242c5f37
Sebi <sebihoorens@gmail.com>
2025-03-16 21:58:12 +0100

    email improvements: logo footer + date

--- test/mailers/previews/guest_mailer_preview.rb
+++ test/mailers/previews/guest_mailer_preview.rb
@@ -2,8 +2,8 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/guest_mailer
 class GuestMailerPreview < ActionMailer::Preview
-  def confirmation_email_attending
-    GuestMailer.confirmation_email(
+  def confirmation_guest_email_attending
+    GuestMailer.confirmation_guest_email(
       Guest.new(
         id: 123,
         first_name: 'John',
@@ -15,8 +15,8 @@ class GuestMailerPreview < ActionMailer::Preview
     )
   end
 
-  def confirmation_email_attending_not_attending
-    GuestMailer.confirmation_email(
+  def confirmation_guest_email_attending_not_attending
+    GuestMailer.confirmation_guest_email(
       Guest.new(
         id: 123,
         first_name: 'John',
@@ -24,6 +24,34 @@ class GuestMailerPreview < ActionMailer::Preview
         email: 'john.doe@example.com',
         token: 'abcdefghiklmnop',
         attending: false
+      )
+    )
+  end
+
+  def confirmation_admin_email_attending
+    GuestMailer.confirmation_admin_email(
+      Guest.new(
+        id: 123,
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john.doe@example.com',
+        token: 'abcdefghiklmnop',
+        attending: true,
+        notes: 'Looking forward to the beers!'
+      )
+    )
+  end
+
+  def confirmation_admin_email_attending_not_attending
+    GuestMailer.confirmation_admin_email(
+      Guest.new(
+        id: 123,
+        first_name: 'John',
+        last_name: 'Doe',
+        email: 'john.doe@example.com',
+        token: 'abcdefghiklmnop',
+        attending: false,
+        notes: 'Looking forward to the beers!'
       )
     )
   end
