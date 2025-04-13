ccccfed75abdaaec887a251201a4b5cf4ffad8d9
Sebi <sebihoorens@gmail.com>
2025-04-13 22:37:52 +0200

    new colours according to our palette + improved locations in emails

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
 class WelcomeController < ApplicationController
-  def index
-    @google_maps_guapulo_url = 'https://maps.app.goo.gl/DTiD1FDRUUtRwWHBA'
-    @google_maps_alcazar_url = 'https://maps.app.goo.gl/C5CQ6MW2hFktH5wA7'
-  end
+  def index; end
 end
